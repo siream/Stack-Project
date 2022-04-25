c7deb2b162c222ac5dd1ca9d31f785a3e5faece8
siream <104356128+siream@users.noreply.github.com>
2022-04-25 20:40:33 +0800

    Update shop.h

--- 项目源文件/shop.h
+++ 项目源文件/shop.h
@@ -9,13 +9,13 @@ void ShopMenu(Hero* player) {
 void Inithero() {
 	ShopMenu();
 	if()
-		printf("\33玩家尚无英雄");
+		printf("\33[y;xH玩家尚无英雄");
 	if () {
-		printf("\33玩家已有英雄");
-		printf("\33名称:");
-		printf("\33ID（每个英雄拥有独立ID，用于比较）");
-		printf("\33技能ID——————子函数catchSkill(),通过传入技能ID以及战斗时双方army栈生产对应效果");
-		printf("\33价格————用钻石买");
+		printf("\33[y;xH玩家已有英雄");
+		printf("\33[y;xH名称:");
+		printf("\33[y;xH ID（每个英雄拥有独立ID，用于比较）");
+		printf("\33[y;xH技能ID——————子函数catchSkill(),通过传入技能ID以及战斗时双方army栈生产对应效果");
+		printf("\33[y;xH价格————用钻石买");
 
 	}
 }
@@ -24,19 +24,19 @@ void Inithero() {
 void Arms() {
 	ShopMenu();
 	if()
-		printf("\33玩家尚无兵种");
+		printf("\33[y;xH玩家尚无兵种");
 	if () {
-		printf("\33玩家已有兵种");
-		printf("\33攻击");
-		printf("\33防御");
-		printf("\33血量");
-		printf("\33照价（战斗中使用金币的购买价格）");
-		printf("\33 ID ————子函数catchDogface()，通过传入ID返回对应属性的小兵");
+		printf("\33[y;xH玩家已有兵种");
+		printf("\33[y;xH攻击");
+		printf("\33[y;xH防御");
+		printf("\33[y;xH血量");
+		printf("\33[y;xH照价（战斗中使用金币的购买价格）");
+		printf("\33[y;xH ID ————子函数catchDogface()，通过传入ID返回对应属性的小兵");
 	}
 }
 
 //Diamonds 钻石
 void Diamonds() {
 	ShopMenu();
-	printf("\33通过战斗获得，可以用来买东西");
+	printf("\33[y;xH通过战斗获得，可以用来买东西");
 }
